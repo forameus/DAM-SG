ff9252411d2c9f4cb9a88c3ca91e5a69fd49620d
forameus <navarrogordilloalberto@gmail.com>
2016-11-25 13:01:16 +0100

    Hecho hasta ahora

---
+++ 07-Almacenamiento/Cookies/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            if (!Request.Cookies.AllKeys.Contains("contador"))
+            {
+                HttpCookie miCookie = new HttpCookie("contador", "0");
+                miCookie.Expires = DateTime.Now.AddDays(7);
+
+                Response.Cookies.Add(miCookie);
+                ViewBag.contador = 0;
+            }
+            else {
+                HttpCookie miCookie = Request.Cookies["contador"];
+
+                ViewBag.contador = Convert.ToInt32(miCookie.Value);
+                //Response.SetCookie(miCookie);
+
+            }
+            return View();
+        }
+
+
+
+        [HttpPost, ActionName("Index")]
+        public ViewResult IndexPost()
+        {
+            HttpCookie miCookie = Request.Cookies["contador"];
+            int contador = Convert.ToInt32(miCookie.Value);
+            contador++;
+            ViewBag.contador = contador;
+
+            miCookie.Value = Convert.ToString(contador);
+            miCookie.Expires.AddDays(7);
+            Response.SetCookie(miCookie);
+
+            return View();
+
+        }
+
+    }
+}
