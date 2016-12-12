20d75758f61e5a967fe52a5b60dacb93342d1967
forameus <navarrogordilloalberto@gmail.com>
2016-12-12 11:59:17 +0100

    Añadido ejercicio JavaScript

---
+++ 10-HolaMundo-Js/Scripts/JavaScript.js
@@ -0,0 +1,25 @@
+﻿function saludo() {
+    document.getElementById("boton").addEventListener("click", boton);
+}
+
+function boton() {
+    var nombre = document.getElementById("txbNombre").value;
+    var apellidos = document.getElementById("txbApellido").value;
+    var ok = true;
+
+    if (nombre === "") {
+        document.getElementById("errorNombre").innerText = "El nombre no puede estar vácio.";
+        ok = false;
+    } else
+        document.getElementById("errorNombre").innerText = "";
+
+    if (apellidos===""){
+        document.getElementById("errorApellido").innerText = "El nombre no puede estar vácio.";
+        ok = false;
+    }else
+        document.getElementById("errorApellido").innerText = "";
+
+    if(ok)
+        document.getElementById("divMensaje").innerText = "Hola "+ nombre+" "+apellidos+", eres un caranchoa.";
+
+}
